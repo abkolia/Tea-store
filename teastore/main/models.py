60ab3c73a65e9f475de9408001406bc8f105eced
abkolia <vkkolyanabdullaev@gmail.com>
2023-02-24 13:29:25 +0700

    Добавил админку и создал базу данных

--- teastore/main/models.py
+++ teastore/main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Articles(models.Model):
+    title = models.CharField('Название товара', max_length=50)
+    desc = models.CharField('Описание', max_length=250)
+    price = models.IntegerField('Цена')
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
